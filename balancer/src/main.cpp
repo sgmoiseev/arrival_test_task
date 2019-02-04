2f0ae45d5416e621692ca347f85ce844f616d757
f0rtung <zadov.vasya@gmail.com>
2019-02-04 11:55:47 +0300

    Catch all exceptions from server

--- balancer/src/main.cpp
+++ balancer/src/main.cpp
@@ -1,14 +1,16 @@
 #include "tcp-server/tcp-server.h"
 
+#include <iostream>
+
 int main(int /*argc*/, char **/*argv */)
 {
     try {
         balancer::tcp_server server{8888};
         server.start();
     } catch (const std::exception &ex ) {
-
+        std::cout << "Server failed with error: " << ex.what() << std::endl;
     } catch (...) {
-
+        std::cout << "Server failed with unknown error" << std::endl;
     }
 
     return 0;
