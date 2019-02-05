60dc394ad67e8eaaf9734218c6048d79125f602f
f0rtung <zadov.vasya@gmail.com>
2019-02-05 16:20:59 +0300

    Catch all errors in tcp session

--- balancer/src/tcp-session/tcp-session.h
+++ balancer/src/tcp-session/tcp-session.h
@@ -30,20 +30,21 @@ namespace balancer {
             , route_map_{route_map}
             , client_buffer_{common::bufferevent_ptr(bufferevent_socket_new(base, socket, BEV_OPT_CLOSE_ON_FREE))}
             , logger_{logger}
-        {}
+        { }
 
         ~tcp_session() = default;
 
         void start() override
         {
-            if(!client_buffer_) {
-                logger_.error("Invalid bufferevent");
-                close_op_();
-                return;
+            try {
+                check_null(client_buffer_, "Invalid client bufferevent");
+                logger_.info("Start new unknown session");
+                start_reading_init_message();
+            } catch (const std::exception &ex) {
+                log_error_and_stop("Can not start session with error: ", ex.what());
+            } catch (...) {
+                log_error_and_stop("Can not start session with unknown error");
             }
-
-            logger_.info("Start new unknown session");
-            start_reading_init_message();
         }
 
         void stop() override
@@ -72,40 +73,51 @@ namespace balancer {
         void read_init_message()
         {
             const auto msg{read_message<proto::init_message>()};
-            logger_.info("We connected with client: ", msg.client_id());
-            start_routing(msg.client_id());
+            if(msg.type() == proto::message_type::init) {
+                logger_.info("We connected with client: ", msg.client_id());
+                start_routing(msg.client_id());
+            } else {
+                const auto type_uint{static_cast<std::uint32_t>(msg.type())};
+                log_error_and_stop("Invalid init message, type is ", type_uint);
+            }
         }
 
         void start_routing(proto::init_message::client_id_t client_id)
         {
             const auto server_it = route_map_.find(client_id);
-            if(route_map_.cend() == server_it) {
-                logger_.error("Have no information about client: ", client_id);
-                stop();
+            if(route_map_.cend() != server_it) {
+                try {
+                    auto &srv{server_it->second};
+                    connect_to_server(srv);
+                    logger_.info("Start routing packets from clietn ", client_id, " to server ", srv);
+                    start_reading_regular_message();
+                } catch (const std::exception &ex) {
+                    log_error_and_stop("Can not start routing, error: ", ex.what());
+                } catch (...) {
+                    log_error_and_stop("Can not start routing with unknown error");
+                }
             } else {
-                auto &srv{server_it->second};
-                connect_to_server(srv);
-                logger_.info("Start routing packets from clietn ", client_id, " to server ", srv);
-                start_reading_regular_message();
+                log_error_and_stop("Have no information about client: ", client_id);
             }
         }
 
         void connect_to_server(const common::remote_server &server)
         {
-            server_buffer_ = common::bufferevent_ptr(bufferevent_socket_new(bufferevent_get_base(client_buffer_.get()),
-                                                                            -1, BEV_OPT_CLOSE_ON_FREE));
-            if(!server_buffer_) {
-                logger_.error("Can not connect to server");
-                stop();
-            }
+            server_buffer_ =
+                    common::bufferevent_ptr(bufferevent_socket_new(bufferevent_get_base(client_buffer_.get()),
+                                                                   -1, BEV_OPT_CLOSE_ON_FREE));
+            check_null(server_buffer_, "Invalid server bufferevent");
 
             auto *buff{server_buffer_.get()};
             bufferevent_setcb(buff, nullptr, nullptr, tcp_session::on_event_cb, this);
-            bufferevent_enable(buff, EV_WRITE);
-            bufferevent_disable(buff, EV_READ);
+            check_result_code(bufferevent_enable(buff, EV_WRITE), "Can not enable server bufferevent for reading");
 
             const auto sock{server.sockaddr()};
-            bufferevent_socket_connect(buff, reinterpret_cast<const sockaddr *>(&sock), sizeof(sock));
+            check_result_code(
+                        bufferevent_socket_connect(buff,
+                                                   reinterpret_cast<const sockaddr *>(&sock),
+                                                   sizeof(sock)),
+                        "Can not start connection procedure to server");
         }
 
         void start_reading_regular_message()
@@ -144,8 +156,8 @@ namespace balancer {
             auto *buff{client_buffer_.get()};
             bufferevent_setcb(buff, r_cb, nullptr, tcp_session::on_event_cb, this);
             bufferevent_setwatermark(buff, EV_READ, lowmark, highmark);
-            bufferevent_enable(buff, EV_READ);
-            bufferevent_disable(buff, EV_WRITE);
+            check_result_code(bufferevent_enable(buff, EV_READ),
+                              "Can not enable client bufferevent for reading");
         }
 
         template<typename msg_t>
@@ -162,6 +174,28 @@ namespace balancer {
         {
             auto *self{static_cast<tcp_session *>(ctx)};
             self->on_next_event(what);
+        }
+
+        template<typename t, typename deleter_t>
+        void check_null(const std::unique_ptr<t, deleter_t> &ptr, const std::string &error_msg)
+        {
+            if(!ptr) {
+                throw std::runtime_error{error_msg};
+            }
+        }
+
+        void check_result_code(int result_code, const std::string &error_msg)
+        {
+            if(-1 == result_code) {
+                throw std::runtime_error{error_msg};
+            }
+        }
+
+        template<typename ...args_t>
+        void log_error_and_stop(args_t&& ...args)
+        {
+            logger_.error(std::forward<args_t>(args)...);
+            stop();
         }
 
     private:
