99daf6ad446e8795fe5f03a2408e9543a44f7156
f0rtung <zadov.vasya@gmail.com>
2019-02-05 19:32:04 +0300

    Fix format

--- logger/src/logger.h
+++ logger/src/logger.h
@@ -11,33 +11,33 @@ namespace logger {
 
     public:
         template<typename ...args_t>
-        void info(args_t&& ...args)
+        void info(args_t &&...args)
         {
             log(log4cplus::INFO_LOG_LEVEL, std::forward<args_t>(args)...);
         }
 
         template<typename ...args_t>
-        void error(args_t&& ...args)
+        void error(args_t &&...args)
         {
             log(log4cplus::ERROR_LOG_LEVEL, std::forward<args_t>(args)...);
         }
 
     private:
         template<typename arg_t, typename ...args_t>
-        void args_to_stream(std::stringstream &stream, arg_t&& arg, args_t&& ...args)
+        void args_to_stream(std::stringstream &stream, arg_t &&arg, args_t &&...args)
         {
             stream << std::forward<arg_t>(arg);
             args_to_stream(stream, std::forward<args_t>(args)...);
         }
 
         template<typename arg_t>
-        void args_to_stream(std::stringstream &stream, arg_t&& arg)
+        void args_to_stream(std::stringstream &stream, arg_t &&arg)
         {
             stream << std::forward<arg_t>(arg) << ".";
         }
 
         template<typename ...args_t>
-        void log(log4cplus::LogLevel level, args_t&& ...args)
+        void log(log4cplus::LogLevel level, args_t &&...args)
         {
             std::stringstream stream;
             args_to_stream(stream, std::forward<args_t>(args)...);
