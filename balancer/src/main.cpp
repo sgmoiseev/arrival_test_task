3da082ea96f8ce9da71666ba15531736d06c33af
f0rtung <zadov.vasya@gmail.com>
2019-02-05 17:14:43 +0300

    Read route map from file

--- balancer/src/main.cpp
+++ balancer/src/main.cpp
@@ -1,19 +1,69 @@
 #include "common.h"
 #include "tcp-server/tcp-server.h"
 
+#include <stdexcept>
+#include <sstream>
 #include <signal.h>
+#include <fstream>
 #include <iostream>
+#include <boost/program_options.hpp>
 
-int main(int /*argc*/, char **/*argv */)
+boost::program_options::variables_map parse_command_line(int argc, const char* const *argv)
+{
+    namespace po = boost::program_options;
+    po::variables_map vm;
+    po::options_description desc{"Options"};
+
+    try {
+        desc.add_options()
+        ("help,h", "Help message")
+        ("route_map,r", po::value<std::string>()->default_value("./route-map.txt"), "File with route map");
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+    } catch (const po::required_option &ex) {
+        if(vm.count("help")) {
+            std::cout << desc << "\n" << std::endl;
+        } else {
+            throw ex;
+        }
+    }
+    return vm;
+}
+
+balancer::route_map read_route_map(const std::string &file_path)
+{
+    balancer::route_map route_map;
+    std::ifstream in_file{file_path};
+    if(in_file) {
+        std::cout << "Route map:" << std::endl;
+        std::string line;
+        while(std::getline(in_file, line)) {
+            std::cout << line << std::endl;
+            std::stringstream stream{line};
+            std::uint32_t client_id; std::string host; std::uint16_t port;
+            stream >> client_id >> host >> port;
+            route_map.emplace(std::make_pair(client_id, common::remote_server{host, port}));
+        }
+    }
+    return route_map;
+}
+
+int main(int argc, char **argv)
 {
     signal(SIGPIPE, SIG_IGN);
     try {
-        balancer::route_map map{
-            {1, common::remote_server{"n1.example.com", 7777}},
-            {2, common::remote_server{"n2.example.com", 9999}},
-            {3, common::remote_server{"n2.example.com", 9999}}
-        };
-        balancer::tcp_server server{8888, map};
+        const auto params{parse_command_line(argc, argv)};
+        if(params.count("help")) {
+            return 0;
+        }
+
+        const std::string route_map_file_path{params["route_map"].as<std::string>()};
+        const auto route_map{read_route_map(route_map_file_path)};
+        if(route_map.empty()) {
+            throw std::invalid_argument{"Empty route map"};
+        }
+
+        balancer::tcp_server server{8888, route_map};
         server.start();
         server.stop();
     } catch (const std::exception &ex ) {
